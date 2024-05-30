97e7856e3f56164ef788e03584de2b7edc4dab7d
José Trigueiro <joseleonardo2000@gmail.com>
2024-05-30 16:16:42 +0100

    missing kafka files from lab

---
+++ src/tukano/utils/kafka/examples/ex1/KafkaReceiver.java
@@ -0,0 +1,21 @@
+package tukano.utils.kafka.examples.ex1;
+
+import java.util.List;
+
+import tukano.utils.kafka.lib.KafkaSubscriber;
+import tukano.utils.kafka.lib.KafkaUtils;
+
+public class KafkaReceiver {
+	private static final String FROM_BEGINNING = "earliest";
+
+	public static void main(String[] args) {
+		KafkaUtils.createTopic(KafkaSender.TOPIC, 1, 1);
+
+		var subscriber = KafkaSubscriber.createSubscriber(KafkaSender.KAFKA_BROKERS, List.of(KafkaSender.TOPIC),
+				FROM_BEGINNING);
+
+		subscriber.start(true, (r) -> {
+			System.out.printf("Topic: %s, offset: %d, value: %s\n", r.topic(), r.offset(), r.value());
+		});
+	}
+}
